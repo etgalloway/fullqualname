a3682791ecb17dbd1d74824c38251aba9a78c066
Eric Galloway <ericgalloway@gmail.com>
2015-09-12 11:20:44 -0700

    Add built-in class method object test

--- tests/test_fullqualname.py
+++ tests/test_fullqualname.py
@@ -46,3 +46,25 @@ def test_builtin_method():
         expected = '__builtin__.list.append'
 
     nose.tools.assert_equals(fullqualname(obj), expected)
+
+
+def test_builtin_classmethod():
+    # Test built-in class method object.
+
+    obj = object.mro
+
+    # Object type is 'builtin_function_or_method'.
+    assert type(obj).__name__ == 'builtin_function_or_method'
+
+    # Object is a method.
+    assert 'built-in method' in repr(obj)
+
+    # Object __self__ attribute is a class.
+    assert inspect.isclass(obj.__self__)
+
+    if sys.version_info >= (3, ):
+        expected = 'builtins.object.mro'
+    else:
+        expected = '__builtin__.object.mro'
+
+    nose.tools.assert_equals(fullqualname(obj), expected)

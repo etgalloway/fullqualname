424980a48e451d1b99397843001bd75fa58e474e
Eric Galloway <ericgalloway@gmail.com>
2015-09-12 10:59:19 -0700

    Add built-in method object test

--- tests/test_fullqualname.py
+++ tests/test_fullqualname.py
@@ -1,5 +1,6 @@
 """Tests for fullqualname."""
 
+import inspect
 import nose
 import sys
 
@@ -21,5 +22,27 @@ def test_builtin_function():
         expected = 'builtins.len'
     else:
         expected = '__builtin__.len'
+
+    nose.tools.assert_equals(fullqualname(obj), expected)
+
+
+def test_builtin_method():
+    # Test built-in method object.
+
+    obj = [1, 2, 3].append
+
+    # Object type is 'builtin_function_or_method'.
+    assert type(obj).__name__ == 'builtin_function_or_method'
+
+    # Object is a method.
+    assert 'built-in method' in repr(obj)
+
+    # Object __self__ attribute is not a class.
+    assert not inspect.isclass(obj.__self__)
+
+    if sys.version_info >= (3, ):
+        expected = 'builtins.list.append'
+    else:
+        expected = '__builtin__.list.append'
 
     nose.tools.assert_equals(fullqualname(obj), expected)
